2bd6e6039022a2bd3fd4ba49c651f8068702ea7e
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2026-02-17 14:46:17 +0100

    Fix error logging

--- main.go
+++ main.go
@@ -208,16 +208,14 @@ func getDynamicLibrary() unsafe.Pointer {
 		if libraryHandle != nil {
 			C.dlerror()
 			C.dlclose(libraryHandle)
-			dlerr := C.dlerror()
-			if dlerr != nil {
-				fmt.Printf("Error when closing dynamic library.\n", dlerr)
+			if err := C.dlerror(); err != nil {
+				fmt.Printf("Error when closing dynamic library.\n", C.GoString(err))
 			}
 		}
 		C.dlerror()
 		libraryHandle = C.dlmopen(C.LM_ID_NEWLM, C.CString(os.Getenv("PKCS11_SUBMODULE")), C.RTLD_NOW|C.RTLD_LOCAL|C.RTLD_DEEPBIND)
-		dlerr := C.dlerror()
-		if dlerr != nil {
-			fmt.Printf("Error when opening dynamic library.\n", dlerr)
+		if err := C.dlerror(); err != nil {
+			fmt.Printf("Error when opening dynamic library.\n", C.GoString(err))
 		}
 		if libraryHandle == nil {
 			return nil
@@ -234,8 +232,8 @@ func getDynamicLibrarySymbol(functionName string) uintptr {
 	}
 	C.dlerror()
 	symbol := C.dlsym(lh, C.CString(functionName))
-	if dlerr != nil {
-		fmt.Printf("Error when getting symbol from dynamic library.\n", dlerr)
+	if err := C.dlerror(); err != nil {
+		fmt.Printf("Error when getting symbol from dynamic library.\n", C.GoString(err))
 	}
 	return uintptr(symbol)
 }
